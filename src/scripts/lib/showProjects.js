75850f72320c01fe5df65cac1467bb5bdf9c0a36
Alex Ballera <alexballera@gmail.com>
2016-02-08 15:21:40 -0430

    Agregado autor y categorias a articles y projects

--- src/scripts/lib/showProjects.js
+++ src/scripts/lib/showProjects.js
@@ -21,21 +21,21 @@ var showProjects = $(() => {
     $projectContainer.find('.loader').remove()
     projects.posts.forEach(function (project) {
       var projectTemplate = template
-      .replace(':name:', project.title)
       .replace(':title:', project.title)
-      .replace(':title article:', project.title)
       .replace(':url:', project.short_URL)
       .replace(':link:', project.short_URL)
       .replace(':link content:', project.short_URL)
       .replace(':image:', project.featured_image)
-      .replace(':summary:', project.excerpt)
       .replace(':image alt:', project.title + ' Image')
+      .replace(':summary:', project.excerpt)
       .replace(':author:', project.author.name)
       .replace(':avatar:', project.author.avatar_URL)
       .replace(':avatar alt:', project.author.name + ' Logo')
       .replace(':profile:', project.author.profile_URL)
       .replace(':profile img:', project.author.profile_URL)
-      .replace(':summary:', project.excerpt)
+      .replace(':category:', project.categories.Proyecto.name)
+      // .replace(':category:', Object.keys(project.categories)[0])
+      .replace(':category2:', Object.keys(project.categories)[1])
 
       var $projectTemplate = $(projectTemplate)
       $projectTemplate.hide()
@@ -46,12 +46,15 @@ var showProjects = $(() => {
   var template = `<section class="content__articles--post" style="display: flex; align-items: center;">
         <div class="content__articles--post--title">
           <a href=":link:" target="_blank"><h3>:title:</h3></a>
-          <picture class="content__articles--post--avatar"
-          style="display:none;align-items:center;">
-          <a href=":profile img:" target="_blank">
-          <img src=":avatar:" alt=":avatar alt:"
-            style="width:25px;height:25px;margin:8px;">
-          </a><a href=":profile:" target="_blank" class="content__articles--post--link"><figcaption style="display:block;outline:none;font-size:80%;color:grey;">:author:</figcaption></a></picture>
+          <picture class="content__articles--post--avatar" style="display:flex;align-items:center;">
+            <a href=":profile img:" target="_blank">
+            <img src=":avatar:" alt=":avatar alt:"
+              style="width:17.5px;height:17.5px;margin:8px;">
+            </a>
+            <a href=":profile:" target="_blank" class="content__articles--post--link">
+              <figcaption style="display:block;outline:none;font-size:70%;color:grey;"><span style="font-weight:700;">:author:</span> | <i class="fa fa-tags"></i> :category:, :category2:</figcaption>
+            </a>
+          </picture>
         </div>
         <picture class="content__articles--post--picture"><a href=":url:" target="_blank" class="content__articles--post--link"><img src=":image:" alt=":image alt:"></a></picture>
         <article class="content__articles--post--text">
@@ -61,7 +64,7 @@ var showProjects = $(() => {
         </section>
   </section>`
 
-  $.ajax('https://public-api.wordpress.com/rest/v1.1/sites/alexballera.com/posts/?number=3')
+  $.ajax('https://public-api.wordpress.com/rest/v1.1/sites/alexballera.com/posts/?category=proyecto&number=5')
       .then((projects) => {
         $projectContainer.find('.loader').remove()
         localStorage.projects = JSON.stringify(projects)
