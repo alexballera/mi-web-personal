b3f89322ff05ad2eca10d609fa7bab8283f52bfc
Alex Ballera <alexballera@gmail.com>
2016-02-25 21:43:30 -0430

    Terminando de optimizar

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -41,7 +41,6 @@ import showCategories from './lib/showCategories'  // Descomentar si se está en
   // Load JS
     var urlJs = [
       '//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-5698df4c26bc43c8'
-      // '//s3.amazonaws.com/downloads.mailchimp.com/js/mc-validate.js'
     ]
     loadJS(urlJs)
 
