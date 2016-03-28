1c34796bde915c060eba377e614be305c7dfc54b
Alex Ballera <alexballera@gmail.com>
2016-03-28 18:34:15 -0430

    Agregado defer

--- src/scripts/lib/loadJS.js
+++ src/scripts/lib/loadJS.js
@@ -3,6 +3,7 @@ var loadJS = (url) => {
     var elem = document.createElement('script')
     elem.src = url[i]
     elem.async = 'async'
+    elem.defer = 'defer'
     elem.type = 'text/javascript'
     document.body.appendChild(elem)
   }
